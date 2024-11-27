a17ed0df2258f53eff29354aca849703c7ecdf58
mrmrh1 <145454529+mrmrh1@users.noreply.github.com>
2024-11-27 12:38:47 +0100

    Sensor Input Arduino Code

---
+++ Distancedata/sensor_input.ino
@@ -0,0 +1,36 @@
+const int trigPin1 = 9;
+const int echoPin1 = 10;
+const int trigPin2 = 11;
+const int echoPin2 = 12;
+
+float duration, distance, duration2, distance2;
+
+void setup() {
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  Serial.begin(9600);
+}
+
+void calcDistance(int trigPin, int echoPin, char name[]) {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  distance = (duration*.0343)/2;
+  Serial.print(name);
+  Serial.println(distance);
+}
+
+void loop(){
+  calcDistance(trigPin1, echoPin1, "Sensor 1: ");
+  delay(500);
+  calcDistance(trigPin2, echoPin2, "Sensor 2: ");
+  delay(500);
+}
+
