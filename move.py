a2aa232414a323dc43b9500da4d95f5b73fde18a
Pjok <julius.hartvig@gmail.com>
2024-12-10 11:56:17 +0100

    Added a smooth_start not tested

--- move.py
+++ move.py
@@ -21,6 +21,21 @@ def smooth_stop(robot, steps):
 
 		time.sleep(0.5 / steps)
 
+def smooth_start(robot, steps, speed):
+	left_value = robot.left_motor.value
+	right_value = robot.right_motor.value
+
+	left_stepsize = (speed - left_value) / steps
+	right_stepsize = (speed - right_value) / steps
+	
+	while left_value < speed or right_value < speed:
+		left_value += left_stepsize
+		right_value += right_stepsize
+
+		robot.left_motor.value = left_value
+		robot.right_motor.value = right_value
+
+		time.sleep(0.5 / steps)
 #Movement
 robot = Robot()
 
