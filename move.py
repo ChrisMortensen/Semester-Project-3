646fca92c670a12765179c37f93902257eaf82e0
Christian Mortensen <christian@svalgaard.net>
2024-12-10 11:24:04 +0100

    Smooth stop function

--- move.py
+++ move.py
@@ -5,9 +5,25 @@ import time
 #pip install Adafruit-MotorHAT
 #pip install sparkfun-qwiic
 
+def smooth_stop(robot, steps):
+	left_value = robot.left_motor.value
+	right_value = robot.right_motor.value
+
+	left_stepsize = left_value / steps
+	right_stepsize = right_value / steps
+	
+	while left_value > 0 or right_value > 0:
+		left_value -= left_stepsize
+		right_value -= right_stepsize
+
+		robot.left_motor.value = left_value
+		robot.right_motor.value = right_value
+
+		time.sleep(0.5 / steps)
+
+#Movement
 robot = Robot()
 
 robot.left(0.3)
 time.sleep(0.5)
-robot.stop()
-
+smooth_stop(robot, 4)
